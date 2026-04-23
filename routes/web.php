921f5a8b0963d4fc8a740685d2092de9def7c295
Agil <asdarrosmawati-maker.com>
2026-04-23 21:21:02 +0800

    feat: penyiapan blade component untuk layout utama

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('produk-tas1', function () {
+    return view('produk-tas1.index', ['title' => 'produk tas']);
+});
+Route::get('produk-tas1/create', function () {
+    return view('produk-tas1.create', ['title' => 'Create produk tas']);
+});
