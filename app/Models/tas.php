429eaf69905f353fa175c5ebe304c91fe3e5d9b5
Agil <asdarrosmawati-maker.com>
2026-04-24 19:56:58 +0800

    feat: pembuatan migration, seeder, dan factory untuk [Produk tasl]

---
+++ app/Models/tas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable(['name', 'merek', 'jenis', 'harga', 'stok'])]
+class tas extends Model
+{
+    /** @use HasFactory<\Database\Factories\TasFactory> */
+    use HasFactory;
+
+    //protected $fillable = ['name', 'merek', 'jenis', 'harga', 'stok'];
+
+    //protected $guarded = ['id'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
+use App\Http\Controllers\TasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('produk-tas1', function () {
-    return view('produk-tas1.index', ['title' => 'produk tas']);
-});
-Route::get('produk-tas1/create', function () {
-    return view('produk-tas1.create', ['title' => 'Create produk tas']);
-});
+Route::get('/produk-tas1', [TasController::class, 'index']);
+Route::get('/produk-tas1/create',  [TasController::class, 'create']);
